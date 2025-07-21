b436867feb033ca337d7dfe6e07bf8a78e1a874b
07-Dili <150236593+07-Dili@users.noreply.github.com>
2025-07-22 02:00:54 +0530

    Initial commit

---
+++ npo-server/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express=require('express')
+const app=express();
+const cors=require('cors');
+const mongoose=require('mongoose');
+app.use(express.json());
+// app.use(cors());
+// app.use(cors({
+//   origin: 'https://npo-website.vercel.app/',
+//   credentials: true
+// }));
+app.use(
+  cors({
+    origin: "*", 
+  })
+);
+
+
+const PORT = process.env.PORT || 5000;
+
+const authroute=require('./routes/userroute');
+const serviceroute=require('./routes/serviceroute');
+const queryroute=require('./routes/queryroute');
+
+app.use('/auth',authroute);
+app.use('/service',serviceroute);
+app.use('/query',queryroute);
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.error(err));
